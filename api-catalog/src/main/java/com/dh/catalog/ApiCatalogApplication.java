586773b90f46ba15746ea74fe062593e732e2bf4
VictoriaMRamirez <victoriaramirez0329@gmail.com>
2022-11-22 00:13:57 -0300

    Fixing problems

--- api-catalog/src/main/java/com/dh/catalog/ApiCatalogApplication.java
+++ api-catalog/src/main/java/com/dh/catalog/ApiCatalogApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ApiCatalogApplication {
 
     public static void main(String[] args) {
